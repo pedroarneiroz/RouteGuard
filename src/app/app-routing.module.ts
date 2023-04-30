e9496122975870c7e4719784d11e82b6aa7f1182
pedroarneiroz <pedroarneiroz@gmail.com>
2023-04-30 18:53:36 -0300

    initial work Route Guardian

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Components Pages
+import { HomeComponent } from './shared/home/home.component';
+import { AccountComponent } from './shared/account/account.component';
+
+//Guards
+import { CanActiveGuard } from './shared/guards/can-active.guard';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'account',
+    component: AccountComponent,
+    canActivate: [CanActiveGuard],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
