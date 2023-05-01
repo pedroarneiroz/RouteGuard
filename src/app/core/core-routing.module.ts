ac08a52bab40979d96d0481c0a42fa851e91462c
pedroarneiroz <pedroarneiroz@gmail.com>
2023-05-01 13:32:07 -0300

    reoganization project and preparation to CanLoad

---
+++ src/app/core/core-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+//Components
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LeadsComponent } from './pages/leads/leads.component';
+
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'leads', component: LeadsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CoreRoutingModule {}
